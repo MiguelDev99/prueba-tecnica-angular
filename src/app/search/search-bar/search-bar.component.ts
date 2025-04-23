58c655137ba6c0978c3d603faa200e3ac864c01a
MiguelDev99 <mojeda@vortexbird.com>
2025-04-23 13:44:36 -0500

    feat(search): implementa barra de búsqueda

---
+++ src/app/search/search-bar/search-bar.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { SpotifyService } from '../../core/services/spotify.service';
+import { DecimalPipe, NgFor, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-search-bar',
+  standalone: true,
+  imports: [NgFor, NgIf, FormsModule, DecimalPipe ],
+  templateUrl: './search-bar.component.html',
+})
+export class SearchBarComponent {
+  searchTerm = '';
+  artists: any[] = [];
+
+  constructor(private spotifyService: SpotifyService) {}
+
+  onSearch() {
+    if (!this.searchTerm.trim()) return;
+
+    this.spotifyService.searchArtists(this.searchTerm).subscribe((res: any) => {
+      this.artists = res.artists.items;
+      console.log('Artistas encontrados:', this.artists);
+    });
+  }
+}
